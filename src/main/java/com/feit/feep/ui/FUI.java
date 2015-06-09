570c4f50685cd1cdf82eae184f4ed57d09540146
zhanggang <157774105@qq.com>
2015-06-10 00:12:05 +0800

    dev fui entity

---
+++ src/main/java/com/feit/feep/ui/FUI.java
@@ -0,0 +1,10 @@
+package com.feit.feep.ui;
+
+/**
+ * Created by ZhangGang on 2015/6/9 0009.
+ */
+public class FUI {
+    public static final String TEXT_LEFT = "left";
+    public static final String TEXT_CENTER = "center";
+    public static final String TEXT_RIGHT = "right";
+}
