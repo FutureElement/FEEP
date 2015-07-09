bfd6bbb76b57763ff36740ab8c0821a562a7838a
zhanggang <157774105@qq.com>
2015-07-09 13:31:39 +0800

    add big data package

---
+++ src/main/java/com/feit/feep/bigData/FeepBD.java
@@ -0,0 +1,9 @@
+package com.feit.feep.bigData;
+
+/**
+ *
+ * Created by zhanggang on 2015/7/9.
+ */
+public class FeepBD {
+
+}
---
+++ src/main/java/com/feit/feep/bigData/mongodb/FeepMongoWarp.java
@@ -0,0 +1,7 @@
+package com.feit.feep.bigData.mongodb;
+
+/**
+ * Created by zhanggang on 2015/7/9.
+ */
+public class FeepMongoWarp {
+}
