1946fc35fa4f10171888bdeaa99b81f4330d87d4
zhanggang <157774105@qq.com>
2015-09-13 11:01:24 +0800

    add deBounce function

--- src/main/webapp/Resource/js/feep.js
+++ src/main/webapp/Resource/js/feep.js
@@ -83,6 +83,20 @@ Feep.top = (function (p, c) {
     }
     return c;
 })(window.parent, window);
+Feep.deBounce = function (func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this, args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
 Feep.resize = function () {
     try {
         if (onSizeChange) {
